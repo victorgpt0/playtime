b9ec9ed93b70acae3977d29736b607a82d7024c6
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2024-09-19 12:00:29 +0300

    constants for the database

---
+++ src/database/constants.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOSTNAME', 'junction.proxy.rlwy.net');
+define('DB_PORT', '24181');
+define('DB_USER', 'root');
+define('DB_PASS', 'VgqklvaWbwUcNZJkFXqoHDdSeIXnFwsk');
+define('DB_NAME', 'railway');
--- src/database/constants_blueprint.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOSTNAME', '');
-define('DB_PORT', '');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
